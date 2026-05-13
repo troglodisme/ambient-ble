cbeee33de74fb462422b96c711052e5730db5c55
Giulio <giulio.ammendola@network.rca.ac.uk>
2026-05-13 09:26:19 +0100

    bump version

--- app.config.js
+++ app.config.js
@@ -7,7 +7,7 @@ export default {
   expo: {
     name: appName,
     slug: 'ambient-ble',
-    version: '0.3.0',
+    version: '0.4.0',
     owner: 'aw-software',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
